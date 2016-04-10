c7d87bbcadc5bf955e07409dbc011c6274d17e03
Eddy Almuiña <e.almuina@lab.matcom.uh.cu>
2016-04-10 19:18:32 -0400

    First commit. Model should be almost completed by now.

---
+++ practicas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Project)
+admin.site.register(Tutor)
+admin.site.register(Workplace)
+admin.site.register(Course)
---
+++ practicas/models.py
@@ -0,0 +1,135 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, verbose_name='usuario')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        verbose_name = 'estudiante'
+
+
+class Major(models.Model):
+    name = models.CharField('nombre', max_length=200)
+    years = models.IntegerField('número de años')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'carrera'
+
+
+class Course(models.Model):
+    start = models.DateField('fecha de inicio')
+    end = models.DateField('Fecha de finalización')
+
+    def __str__(self):
+        return '%s-%s' % (self.start.year, self.end.year)
+
+    class Meta:
+        verbose_name = 'curso'
+
+
+class RegisteredStudent(models.Model):
+    student = models.ForeignKey('Student', verbose_name='estudiante')
+    course = models.ForeignKey('Course', verbose_name='curso')
+    major = models.ForeignKey('Major', verbose_name='carrera')
+
+    group = models.CharField('grupo', max_length=200)
+
+    def __str__(self):
+        return str(self.student)
+
+    class Meta:
+        verbose_name = 'estudiante registrado'
+        verbose_name_plural = 'estudiantes registrados'
+
+
+class Project(models.Model):
+    course = models.ForeignKey('Course', verbose_name='curso')
+    tutor = models.ForeignKey('Tutor')
+
+    name = models.CharField('nombre', max_length=200)
+    description = models.CharField('descripción', max_length=200)
+    report = models.FileField('informe del tutor')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'proyecto'
+
+
+class Request(models.Model):
+    reg_student = models.ForeignKey('RegisteredStudent', verbose_name='estudiante registrado')
+    project = models.ForeignKey('Project', verbose_name='proyecto')
+
+    priority = models.IntegerField('prioridad')
+    checked = models.BooleanField('confirmación del tutor')
+
+    class Meta:
+        verbose_name = 'solicitud'
+        verbose_name_plural = 'solicitudes'
+
+
+class Participation(models.Model):
+    reg_student = models.OneToOneField('RegisteredStudent', verbose_name='estudiante registrado')
+    project = models.ForeignKey('Project', verbose_name='proyecto')
+
+    grade = models.IntegerField('calificación')
+    report = models.FileField('informe del estudiante')
+    tutor_report = models.FileField('informe del tutor')
+
+    class Meta:
+        verbose_name = 'participación'
+        verbose_name_plural = 'participaciones'
+
+
+class Requirement(models.Model):
+    project = models.ForeignKey('Project', verbose_name='proyecto')
+    major = models.ForeignKey('Major', verbose_name='carrera')
+
+    students_count = models.IntegerField('cantidad de estudiantes')
+    year = models.IntegerField('año')
+
+    class Meta:
+        verbose_name = 'requisito'
+
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User, verbose_name='usuario')
+
+    workplace = models.ForeignKey('Workplace', verbose_name='centro de trabajo')
+
+    DOCTOR = 'DR'
+    MASTER = 'MSC'
+    BACHELOR = 'LIC'
+    CATEGORIES = ((DOCTOR, 'Doctor'),
+                  (MASTER, 'Master'),
+                  (BACHELOR, 'Licenciado'))
+
+    category = models.CharField('categoría científica', choices=CATEGORIES, max_length=3)
+    job = models.CharField('puesto', max_length=200)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        verbose_name_plural = 'tutores'
+
+
+class Workplace(models.Model):
+    address = models.CharField('dirección', max_length=200)
+    name = models.CharField('nombre', max_length=200)
+    phone = models.IntegerField('teléfono')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'centro de trabajo'
+        verbose_name_plural = 'centros de trabajo'
