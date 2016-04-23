a3167e585358e0bdb46647655db044c4d204795c
Eddy Almuiña <e.almuina@lab.matcom.uh.cu>
2016-04-22 23:00:32 -0400

    Modified population script.

--- populate_practicas.py
+++ populate_practicas.py
@@ -64,24 +64,6 @@ def populate():
     jose = add_student(jose)
 
     add_registered_student(student=eddy,
-                           course=c14_15,
-                           major=cs,
-                           year=2,
-                           group='C213')
-
-    add_registered_student(student=miguel,
-                           course=c14_15,
-                           major=cs,
-                           year=2,
-                           group='C213')
-
-    add_registered_student(student=jose,
-                           course=c14_15,
-                           major=cs,
-                           year=2,
-                           group='C213')
-
-    add_registered_student(student=eddy,
                            course=c15_16,
                            major=cs,
                            year=3,
@@ -98,6 +80,24 @@ def populate():
                            major=cs,
                            year=3,
                            group='C312')
+
+    eddy = add_registered_student(student=eddy,
+                                  course=c14_15,
+                                  major=cs,
+                                  year=2,
+                                  group='C213')
+
+    miguel = add_registered_student(student=miguel,
+                                    course=c14_15,
+                                    major=cs,
+                                    year=2,
+                                    group='C213')
+
+    jose = add_registered_student(student=jose,
+                                  course=c14_15,
+                                  major=cs,
+                                  year=2,
+                                  group='C213')
 
     oscar = add_tutor(user=oscar,
                       workplace=matcom,
@@ -109,15 +109,49 @@ def populate():
                        category='MSC',
                        job='Profesor')
 
-    add_project(course=c14_15,
-                tutor=oscar,
-                name='Scrapping',
-                description='Implementación de un sistema de búsqueda de bibliografía digital.')
+    scrapping = add_project(course=c14_15,
+                            tutor=oscar,
+                            name='Scrapping',
+                            description='Implementación de un sistema de búsqueda de bibliografía digital.')
 
-    add_project(course=c14_15,
-                tutor=somoza,
-                name='Strings',
-                description='Investigación sobre algoritmos de manipulación de cadenas de caracteres.')
+    strings = add_project(course=c14_15,
+                          tutor=somoza,
+                          name='Strings',
+                          description='Investigación sobre algoritmos de manipulación de cadenas de caracteres.')
+
+    add_requirement(project=strings,
+                    major=cs,
+                    year=2,
+                    students_count=2)
+
+    add_requirement(project=scrapping,
+                    major=cs,
+                    year=2,
+                    students_count=3)
+
+    add_request(reg_student=eddy,
+                project=strings,
+                priority=0)
+
+    add_request(reg_student=miguel,
+                project=strings,
+                priority=0)
+
+    add_request(reg_student=jose,
+                project=scrapping,
+                priority=0)
+
+    add_participation(reg_student=eddy,
+                      project=strings,
+                      grade=5)
+
+    add_participation(reg_student=miguel,
+                      project=strings,
+                      grade=5)
+
+    add_participation(reg_student=jose,
+                      project=scrapping,
+                      grade=4)
 
 
 def add_user(username, first_name, last_name, email, password):
@@ -187,6 +221,32 @@ def add_workplace(address, name, phone='0'):
     w.address = address
     w.save()
     return w
+
+
+def add_request(reg_student, project, priority, checked=True):
+    req = Request.objects.get_or_create(reg_student=reg_student,
+                                        project=project)[0]
+    req.priority = priority
+    req.checked = checked
+    req.save()
+    return req
+
+
+def add_participation(reg_student, project, grade):
+    part = Participation.objects.get_or_create(reg_student=reg_student,
+                                               project=project)[0]
+    part.grade = grade
+    part.save()
+    return part
+
+
+def add_requirement(project, major, year, students_count):
+    req = Requirement.objects.get_or_create(project=project,
+                                            major=major,
+                                            year=year,
+                                            students_count=students_count)[0]
+    req.save()
+    return req
 
 
 # Start execution here!
